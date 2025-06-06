62e66265fba4c37947c971bb56703bbbbdd7acef
kausikaaa <kauss0806@gmail.com>
2025-06-06 18:16:59 +0530

    check : Loaded the dataset

--- SpamMail-Detection/main.py
+++ SpamMail-Detection/main.py
@@ -8,3 +8,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
+
+#Load the dataset
+df=pd.read_csv('data/spam.csv',encoding='latin-1')
+print(df.head())
+df = df[['v1', 'v2']]
+df.columns = ['label', 'message']  # Rename for clarity
+
+# Check shape and info
+print(df.shape)
+print(df.info())
